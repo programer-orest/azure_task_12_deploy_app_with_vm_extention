08f6ea3b9297c3bfed679de520fa19c907d3244d
Orest Yakymiv <akimivorest@gmail.com>
2025-06-03 14:39:20 +0300

    fixed error

--- install-app.sh
+++ install-app.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Script to silently install and start the todo web app on the virtual machine. 
-# Note that all commands bellow are without sudo - that's because extention mechanism 
+# Note that all commands bellow are without sudo - that's because extention mechanism
 # runs scripts under root user.
-git clone https://github.com/programer-orest/azure_task_12_deploy_app_with_vm_extention
+git clone https://github.com/programer-orest/azure_task_12_deploy_app_with_vm_extention.git
 
 # install system updates and isntall python3-pip package using apt. '-yq' flags are
 # used to suppress any interactive prompts - we won't be able to confirm operation
